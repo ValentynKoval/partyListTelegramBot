2da0ac4b9859a0aefd289487eb7a7c2706ecf1d8
Mafia0331 <J.K.play2015@gmail.com>
2024-03-14 14:47:09 +0200

    create project

---
+++ src/main/java/org/spring/listforparty/ListForPartyApplication.java
@@ -0,0 +1,13 @@
+package org.spring.listforparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListForPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListForPartyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spring/listforparty/ListForPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.listforparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListForPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
